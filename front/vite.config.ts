174559894875c1b7879b2ed21bcf8c12afee864a
Amer-Zakaria <ameruniversity1234@gmail.com>
2025-04-04 14:43:53 +0300

    Trying PWA

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,6 +1,34 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Romance Dashboard",
+        short_name: "Romance_Dash",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#000000",
+      },
+      workbox: {
+        // Cache all static assets on first load
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) =>
+              request.destination === "script" ||
+              request.destination === "style",
+            handler: "CacheFirst",
+            options: {
+              cacheName: "static-assets",
+              expiration: { maxEntries: 50, maxAgeSeconds: 30 * 24 * 60 * 60 },
+            },
+          },
+        ],
+      },
+    }),
+  ],
 });
